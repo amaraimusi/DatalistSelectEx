522e9af46f0569beeafc91d5f14b57afbd2b0922
amaraimusi <amaraimusi@gmail.com>
2023-02-05 22:58:29 +0900

    サンプルのリンク先をREADMEに追記

--- 01_upload.sh
+++ 01_upload.sh
@@ -1,7 +1,7 @@
 ﻿#!/bin/sh
 echo 'ソースコードを差分アップロードします。'
 
-rsync -auvz ../DatalistSelectEx amaraimusi@amaraimusi.sakura.ne.jp:www
+rsync -auvz ../DatalistSelectEx amaraimusi@amaraimusi.sakura.ne.jp:www/example_js_k
 
 
 echo "------------ 送信完了"
